47791c81cf76cb3e30aba933f082f3f0ac6e6fb0
aiwasaki126 <90945584+aiwasaki126@users.noreply.github.com>
2023-10-25 06:30:08 +0900

    add: provider example

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,22 @@
-provider "scaffolding" {
-  # example configuration here
+terraform {
+  required_providers {
+    natureremo = {
+      source = "registry.terraform.io/hashicorp/natureremo"
+    }
+  }
+}
+
+provider "natureremo" {
+  access_token = var.access_token
+}
+
+data "natureremo_user" "me" {}
+
+output "me" {
+  value = data.natureremo_user.me
+}
+
+variable "access_token" {
+  type      = string
+  sensitive = true
 }
