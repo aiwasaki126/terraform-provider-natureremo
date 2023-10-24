e6824fda3910315093185fbca0b77d628a64e429
aiwasaki126 <90945584+aiwasaki126@users.noreply.github.com>
2023-10-25 06:39:09 +0900

    remove: acc test for user_resource

--- internal/provider/user_data_source_test.go
+++ internal/provider/user_data_source_test.go
@@ -1,21 +1,21 @@
 package provider
 
-import (
-	"testing"
+// import (
+// 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-)
+// 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+// )
 
-func TestAccUserDatasource(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				Config: providerConfig + `data "natureremo_user" "me" {}`,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.natureremo_user.me", "nickname", "TEST"),
-				),
-			},
-		},
-	})
-}
+// // func TestAccUserDatasource(t *testing.T) {
+// // 	resource.Test(t, resource.TestCase{
+// // 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+// // 		Steps: []resource.TestStep{
+// // 			{
+// // 				Config: providerConfig + `data "natureremo_user" "me" {}`,
+// // 				Check: resource.ComposeAggregateTestCheckFunc(
+// // 					resource.TestCheckResourceAttr("data.natureremo_user.me", "nickname", "TEST"),
+// // 				),
+// // 			},
+// // 		},
+// // 	})
+// // }
