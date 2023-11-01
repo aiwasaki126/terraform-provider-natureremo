878a2572c94c03246242e24a52a0b6a4d3df3d7e
aiwasaki126 <90945584+aiwasaki126@users.noreply.github.com>
2023-11-02 06:44:16 +0900

    update: comment out provider test

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -3,31 +3,24 @@
 
 package provider
 
-import (
-	"testing"
+// // testAccProtoV6ProviderFactories are used to instantiate a provider during
+// // acceptance testing. The factory function will be invoked for every Terraform
+// // CLI command executed to create a provider server to which the CLI can
+// // reattach.
+// var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
+// 	"natureremo": providerserver.NewProtocol6WithError(New("test")()),
+// }
 
-	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
-)
+// func testAccPreCheck(t *testing.T) {
+// 	// You can add code here to run prior to any test case execution, for example assertions
+// 	// about the appropriate environment variables being set are common to see in a pre-check
+// 	// function.
+// }
 
-// testAccProtoV6ProviderFactories are used to instantiate a provider during
-// acceptance testing. The factory function will be invoked for every Terraform
-// CLI command executed to create a provider server to which the CLI can
-// reattach.
-var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-	"natureremo": providerserver.NewProtocol6WithError(New("test")()),
-}
-
-func testAccPreCheck(t *testing.T) {
-	// You can add code here to run prior to any test case execution, for example assertions
-	// about the appropriate environment variables being set are common to see in a pre-check
-	// function.
-}
-
-const (
-	providerConfig = `
-	provider "natureremo" {
-		access_token = "ACCESS_TOKEN"
-	}
-	`
-)
+// const (
+// 	providerConfig = `
+// 	provider "natureremo" {
+// 		access_token = "ACCESS_TOKEN"
+// 	}
+// 	`
+// )
