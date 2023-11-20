b34021f9e0cc278b9a1cc7ff20bbadd6b9071de0
aiwasaki126 <90945584+aiwasaki126@users.noreply.github.com>
2023-11-21 07:08:01 +0900

    add: device usecases

---
+++ internal/client/usecase/delete_device.go
@@ -0,0 +1,27 @@
+package usecase
+
+import (
+	"context"
+	"terraform-provider-natureremo/internal/client/domain/repository"
+)
+
+type DeleteDevice struct {
+	repositoryDevice repository.Device
+}
+
+func NewDeleteDevice(r repository.Device) *DeleteDevice {
+	return &DeleteDevice{
+		repositoryDevice: r,
+	}
+}
+
+func (u *DeleteDevice) DeleteDevice(ctx context.Context, id string) error {
+	d, err := u.repositoryDevice.GetDevice(ctx, id)
+	if err != nil {
+		return err
+	}
+	if err := u.repositoryDevice.DeleteDevice(ctx, d); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ internal/client/usecase/update_device.go
@@ -0,0 +1,41 @@
+package usecase
+
+import (
+	"context"
+	"terraform-provider-natureremo/internal/client/domain/repository"
+	"terraform-provider-natureremo/internal/client/domain/valueobject"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type UpdateDevice struct {
+	repositoryDevice repository.Device
+}
+
+func NewUpdateDevice(r repository.Device) *UpdateDevice {
+	return &UpdateDevice{
+		repositoryDevice: r,
+	}
+}
+
+func (u *UpdateDevice) UpdateDevice(ctx context.Context, deviceDto DeviceDto) (*DeviceDto, error) {
+	if err := validator.New().Struct(deviceDto); err != nil {
+		return nil, err
+	}
+	d, err := u.repositoryDevice.GetDevice(ctx, deviceDto.Id)
+	if err != nil {
+		return nil, err
+	}
+	newSetting, err := valueobject.NewDeviceSetting(deviceDto.Name, deviceDto.HumidityOffset, deviceDto.TemperatureOffset)
+	if err != nil {
+		return nil, err
+	}
+	if err := d.UpdateSetting(*newSetting); err != nil {
+		return nil, err
+	}
+	d, err = u.repositoryDevice.UpdateDevice(ctx, d)
+	if err != nil {
+		return nil, err
+	}
+	return newDeviceDtoFromEntity(d), nil
+}
